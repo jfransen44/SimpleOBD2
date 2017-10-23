5c87fc1f841248fdcd9c5e1ddb344d1a0192ca0f
Jeremy Fransen <jeremyfransen@Jeremys-MacBook-Pro.local>
2017-10-22 19:38:58 -0700

    initial commit

---
+++ OBC_Connect.py
@@ -0,0 +1,52 @@
+import serial
+import time
+import logging
+
+logging.basicConfig(filename="sampleOut.txt", level=logging.DEBUG)
+
+logging.BASIC_FORMAT
+
+
+ser = serial.Serial("/dev/tty.OBDII-Port", 38400, timeout=1)
+total_speed = 0
+high_speed = 0
+total_time = 0
+cur_speed = 0
+cur_gear = 0
+cur_RPM = 0
+high_RPM = 0
+
+def convert_to_decimal(hex):
+    return round(float(int("0x" + hex, 0)), 2)
+
+def get_speed():
+    ser.write("01 0D \r") # 01 - show current data, 0D - Vehicle Speed
+    speed_hex = ser.readline().split(" ") #convert response to array
+    speed_KPH = convert_to_decimal(speed_hex[4]) #convert relevant portion of hex response to decimal
+    return speed_KPH
+
+def KPH_to_MPH(speed_KPH):
+    return round(speed_KPH / 1.60934, 2) #round to 2 decimal points
+
+def get_RPM():
+    ser.write("01 0C \r") # 01 show current data, 0C - RPM
+    RPM_hex = ser.readline().split(" ")
+    param_1 = convert_to_decimal(RPM_hex[4])
+    param_2 = convert_to_decimal(RPM_hex[5])
+    return round(((param_1 * 256 + param_2) / 4), 2)
+
+
+while cur_speed == 0:
+    cur_speed = get_speed()
+
+t0 = time.time()
+while cur_speed != 0:
+    cur_speed = get_speed()
+    high_speed = max(high_speed, cur_speed)
+    cur_RPM = get_RPM()
+    high_RPM = max(high_RPM, cur_RPM)
+    logging.info("Speed: %2.2f KM/H, %2.2f MP/H @ %5.2f RPM" % (cur_speed, KPH_to_MPH(cur_speed), cur_RPM))
+    time.sleep(.5)
+
+t0 = time.time() - t0
+logging.info("Test complete. Time elapsed: %s seconds" % t0)
